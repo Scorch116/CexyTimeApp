b4b62367e10773a1fa1d65fee21ed002c7cbcdbd
Scorch116 <e.dakin@hotmail.com>
2021-03-27 16:07:45 +0000

    implementing Camera frame for capture

--- Barcode.py
+++ Barcode.py
@@ -3,3 +3,9 @@ from pyzbar.pyzbar  import decode # libary to read one-demensional barcodes , us
 import cv2
 import time
 
+# capture image with camera window for testing purpose , dont have barcode scanner
+# Window size could be altered for main application
+capture = cv2.VideoCapture(0)
+capture.set(3, 640) # demension for window width
+capture.set(4, 480) # demension for window height
+camera = True
