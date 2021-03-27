510a46852607f7525852271dbca03d6ba67fcfe7
Scorch116 <e.dakin@hotmail.com>
2021-03-27 16:11:57 +0000

    while loop added with for loop + first conditional

--- Barcode.py
+++ Barcode.py
@@ -11,3 +11,19 @@ capture.set(4, 480) # demension for window height
 camera = True
 
 staff_tags =[] # will be used to add ID of staff for testing , should be hardcoded.
+
+while camera == True: # camera will stay open for scanning (testing make sure to change to false for conditioing later in application)
+    success, frame = capture.read()  # frame is the data we need
+
+    for code in decode(frame):  # for loop used for capturing data from the frame 
+        if code.data.decode('utf-8') in staff_tags: 
+
+            tStamp = time.time() # used for time stamping 
+
+            print('Recorded time :', tStamp) 
+            print('Welcome')
+            print(code.type)  # testing purpose will show the type of data
+            print(code.data.decode('utf-8')) # will decode and dispaly data , decoding using utf-8
+
+            time.sleep(3) # set time til next scan , not gonna set for 1 second cause it scans way to quick.
+
