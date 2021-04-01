d2f610a27d0ba7d2b96a9884413b30c3e5a5722c
Scorch116 <e.dakin@hotmail.com>
2021-04-01 18:31:25 +0100

    Window Created , with title

--- login.py
+++ login.py
@@ -21,7 +21,14 @@ mycursor =mydb.cursor()
 #login class , init definiton for varible access.
 class login:
     def __init__(self,root) :
-        pass
+        self.root=root
+        self.root.title("CExYtime")
+        self.root.geometry("1900x1000+0+0") # size of window , width then height
+
+        #Create label for title in frame 
+        title = Label(self.root, text="CExYtime Login", font=("verdana",40,"bold"), bg="blue", fg="white")
+        #Using pack method to decare position of widgets. Fill with X will fill the window with the brackground colour for tidyness.
+        title.pack(side=TOP, fill=X)
 
 
 root=Tk()
