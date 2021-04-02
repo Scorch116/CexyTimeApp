86a3370c41c4fb15501230bfd274cd3d3093afb3
Scorch116 <e.dakin@hotmail.com>
2021-04-02 20:10:45 +0100

    LABELS AND LOGIN BUTTON ADDED TO GUI

--- login.py
+++ login.py
@@ -24,7 +24,7 @@ class login:
     def __init__(self,root) :
         self.root=root
         self.root.title("CExYtime")
-        self.root.geometry("1900x1000+0+0") # size of window , width then height
+        self.root.geometry("1700x1000+0+0") # size of window , width then height
 
         #Create label for title in frame 
         title = Label(self.root, text="CExYtime Login", font=("verdana",40,"bold"), bg="blue", fg="white")
@@ -34,9 +34,40 @@ class login:
         #Declaring main side frame 
         mainFrame = Frame(self.root, bd="4", bg="blue", relief=RIDGE)
         #must "place" for visibility.
-        mainFrame.place(x=650, y=85, width=600,height=700)
+        mainFrame.place(x=600, y=85, width=600,height=700)
 
+        #label for Username 
+        #using grid starting from row3
+        L_username=Label(mainFrame, text="Username", bg="blue", fg="white", font=("verdana",12,"bold"))
+        L_username.grid(row=3, column=1, padx=75, pady=50, sticky="w")
+        #textbox entry for username
+        T_username=Entry(mainFrame,font=("verdana",12,"bold"), bd=5, relief=GROOVE)
+        T_username.grid(row=3, column=2, padx=25, pady= 50, sticky="w" )
+
+        #label for password
+        #using grid starting from row 4
+        L_password=Label(mainFrame, text="Password", bg="blue", fg="white", font=("verdana",12,"bold"))
+        L_password.grid(row=4, column=1, padx=75, pady=10, sticky="w")
+        #textbox entry for password
+        T_password=Entry(mainFrame,font=("verdana",12,"bold"), bd=5, relief=GROOVE)
+        T_password.grid(row=4, column=2, padx=25, pady= 10, sticky="w" )
+
+        #Frame for buttons added
+
+        buttonFrame = Frame(mainFrame, bd="4", bg="blue", relief=RIDGE)
+        buttonFrame.place(x=35, y=350, width=500,height=150)
+
+        #buttons
+        loginbtn = Button(buttonFrame, text="LOGIN", width=65,height=7,).grid(row=0 , column= 0 , padx=10 , pady=15)
         
+
+
+
+    
+    def login():
+        pass
+
+
 
 
       
