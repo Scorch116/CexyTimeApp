c8f857a3684d9113fdab0a6d06a1a3023bdbe931
Scorch116 <e.dakin@hotmail.com>
2021-04-06 13:11:27 +0100

    added sql connector

--- staffindex.py
+++ staffindex.py
@@ -6,3 +6,13 @@ from tkinter import *
 from tkinter import ttk
 import mysql.connector
 from tkinter import messagebox
+
+#sql connector 
+mydb = mysql.connector.connect(
+    host="localhost",
+    user="admin",
+    passwd="admin",
+    database ="cexydatabase"
+)
+
+mycursor =mydb.cursor()
