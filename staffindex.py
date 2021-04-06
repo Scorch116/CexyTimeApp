37dc0118cdd7d1a4f59d83cce35a00bf948ca402
Scorch116 <e.dakin@hotmail.com>
2021-04-06 13:17:30 +0100

    defined class and size for GUI

--- staffindex.py
+++ staffindex.py
@@ -16,3 +16,15 @@ mydb = mysql.connector.connect(
 )
 
 mycursor =mydb.cursor()
+
+class rota:
+
+    def __init__(self,rotaindex):
+        self.rotaindex = rotaindex
+        self.rotaindex.title("CExYtime")
+        self.rotaindex.geometry("1900x700+0+0") # size of window , width then height
+
+        #Create label for title in frame 
+        title = Label(self.rotaindex, text="CExYtime Rota", font=("verdana",40,"bold"), bg="blue", fg="white")
+        #Using pack method to decare position of widgets. Fill with X will fill the window with the brackground colour for tidyness.
+        title.pack(side=TOP, fill=X)
