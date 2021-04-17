1a05eed0ef332fbf5f1d1743929fb7fa8256bd84
Scorch116 <e.dakin@hotmail.com>
2021-04-17 13:49:11 +0100

    testing security check for shift change

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ from tkinter.ttk import *
 from tkinter import *
 from tkinter import messagebox
 
-
+#varibale for SQl connector
 mydb = mysql.connector.connect(
     host="localhost",
     user="admin",
@@ -14,36 +14,53 @@ mydb = mysql.connector.connect(
 
 mycursor =mydb.cursor()
 
-root=Tk()
-root.title("Student Data")
-root.geometry("1200x700")
 
-label12=Label(root).grid(row=8,column=5)
-label1=Label(root,text="RollNo",width=20,height=2,bg="pink").grid(row=0,column=0)
-label2=Label(root,text="First_Name",width=20,height=2,bg="pink").grid(row=1,column=0)
-label3=Label(root,text="Last_Name",width=20,height=2,bg="pink").grid(row=2,column=0)
-label4=Label(root,text="Phone_Number",width=20,height=2,bg="pink").grid(row=3,column=0)
-label5=Label(root,text="City",width=20,height=2,bg="pink").grid(row=4,column=0)
-label6=Label(root,text="State",width=20,height=2,bg="pink").grid(row=5,column=0)
-label7=Label(root,text="Age",width=20,height=2,bg="pink").grid(row=6,column=0)
-label8=Label(root,width=10,height=2).grid(row=7,column=2)
-label9=Label(root,width=10,height=2).grid(row=7,column=4)
-label10=Label(root,width=10,height=2).grid(row=7,column=6)
-label11=Label(root,width=10,height=2).grid(row=7,column=8)
-e1=Entry(root,width=30,borderwidth=8)
-e1.grid(row=0,column=1)
-e2=Entry(root,width=30,borderwidth=8)
-e2.grid(row=1,column=1)
-e3=Entry(root,width=30,borderwidth=8)
-e3.grid(row=2,column=1)
-e4=Entry(root,width=30,borderwidth=8)
-e4.grid(row=3,column=1)
-e5=Entry(root,width=30,borderwidth=8)
-e5.grid(row=4,column=1)
-e6=Entry(root,width=30,borderwidth=8)
-e6.grid(row=5,column=1)
-e7=Entry(root,width=30,borderwidth=8)
-e7.grid(row=6,column=1)
+class accountCheck:
+    def __init__(self,check) :
+        self.check=check
+        self.check.title("CExYtime")
+        self.check.geometry("430x220+0+0") # size of window , width then height
+
+        #Declaring main side frame 
+        mainFrame = Frame(self.check, bd="4", bg="black", relief=RIDGE)
+        #must "place" for visibility.
+        mainFrame.place(x=0, y=0, width=430,height=220)
+
+        #label for first ID
+        L_first_username=Label(mainFrame, text="First ID", bg="black", fg="white", font=("verdana",12,"bold"))
+        L_first_username.grid(row=0, column=0, padx=25, pady=15, sticky="w")
+        #textbox entry for first ID
+        self.T_first_username=Entry(mainFrame,font=("verdana",12,"bold"), bd=5, relief=GROOVE)
+        self.T_first_username.grid(row=0, column=1, padx=25, pady= 15, sticky="w" )
+
+        #label for second ID
+        L_second_username=Label(mainFrame, text="Second ID", bg="black", fg="white", font=("verdana",12,"bold"))
+        L_second_username.grid(row=1, column=0, padx=25, pady=15, sticky="w")
+        #textbox entry for second ID
+        self.T_second_username=Entry(mainFrame,font=("verdana",12,"bold"), bd=5, relief=GROOVE)
+        self.T_second_username.grid(row=1, column=1, padx=25, pady= 15, sticky="w" )
+        
+
+        #Frame for buttons added
+
+        buttonFrame = Frame(mainFrame, bd="4", bg="black")
+        buttonFrame.place(x=10, y=150, width=400,height=50)
+        #buttons
+        loginbtn = Button(buttonFrame, text="CONFIRM", width=55,height=2,).grid(row=4 , column= 0 , padx=0 , pady=0)
+
+    #fucntion to confirm ID of staff for shift exchange 
+    def confirmID(self):
+        #declaring varibles for the username entry.
+        ID1 = self.T_first_username.get()
+        ID2 = self.T_second_username.get()
+        #delete fields 
+        self.T_first_username.delete(0,END)
+        self.T_second_username.delete(0,END)
+        #sql select statement to retrieve username from database
+        
 
 
-root.mainloop()
+
+check=Tk()
+object=accountCheck(check)
+check.mainloop()
